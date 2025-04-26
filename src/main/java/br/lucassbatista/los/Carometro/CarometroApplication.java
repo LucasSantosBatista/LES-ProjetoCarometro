55afb21fbb688a23c3d4b55805b02e7da1db4c57
Lucas S. Batista <142185716+LucasSantosBatista@users.noreply.github.com>
2025-04-26 10:13:13 -0300

    26/04/2025

---
+++ src/main/java/br/lucassbatista/los/Carometro/CarometroApplication.java
@@ -0,0 +1,13 @@
+package br.lucassbatista.los.Carometro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarometroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarometroApplication.class, args);
+	}
+
+}
